81161cc094f8f38040f81c5db5f0e06ea80ae806
kevinmolina-io <kevmo1987@gmail.com>
2022-01-28 14:06:35 -0800

    console logs test

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,7 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find()
 
+    console.log("Getting posts...")
     res.status(200).json(postMessages)
   } catch (error) {
     res.status(404).json({ message: error.message })
@@ -15,7 +16,7 @@ export const createPost = async (req, res) => {
   const post = req.body
 
   const newPost = new PostMessage(post)
-
+  console.log("Creating posts...")
   try {
     await newPost.save()
 
