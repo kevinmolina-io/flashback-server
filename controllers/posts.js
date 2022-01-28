aadc2150ace61fdac2ac3f5846ca4104abf6242d
kevinmolina-io <kevmo1987@gmail.com>
2022-01-28 14:54:42 -0800

    cleanup

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,6 @@ export const getPosts = async (req, res) => {
   try {
     // query post
     const postMessages = await PostMessage.find()
-    console.log("getting posts");
     res.status(200).json(postMessages)
   } catch (error) {
     res.status(404).json({ message: error.message })
@@ -21,9 +20,6 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save()
-    // saving post...
-    console.log("saving posts...")
-    console.log("...")
 
     res.status(201).json(newPost)
   } catch (error) {
