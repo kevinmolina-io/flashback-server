fcda0607cced7c868d516e65c8098cc5211a4e2e
kevinmolina-io <kevmo1987@gmail.com>
2022-01-28 17:35:04 -0800

    Revert "bug fix a"
    This reverts commit cba88f1311dcd3c4b8334af2bc17e75c3a388f2a.

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,6 @@ export const getPosts = async (req, res) => {
   try {
     // query post
     const postMessages = await PostMessage.find()
-    console.log("GET BUG FIX A")
     res.status(200).json(postMessages)
   } catch (error) {
     res.status(404).json({ message: error.message })
@@ -19,7 +18,7 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save()
-    console.log("CREATE BUG FIX A")
+
     res.status(201).json(newPost)
   } catch (error) {
     res.status(409).json({ message: error.message })
