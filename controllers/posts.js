fbc4c6c32d0b7e68ba693be38da49c64fc919081
Kevin Molina <39111772+kevinmolina-io@users.noreply.github.com>
2022-01-28 17:58:24 -0800

    removed all console logs and uneccessary comments (#20)

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,12 +2,8 @@ import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
   try {
-    // query post
     const postMessages = await PostMessage.find()
-    console.log("GET BUG FIX A")
     res.status(200).json(postMessages)
-    console.log("additional logging A")
-    console.log("successful get Posts A")
   } catch (error) {
     res.status(404).json({ message: error.message })
   }
@@ -21,11 +17,7 @@ export const createPost = async (req, res) => {
 
   try {
     await newPost.save()
-    console.log("CREATE BUG FIX A")
     res.status(201).json(newPost)
-    console.log("additional logging A")
-    console.log("successful create post A")
-    console.log("extra logging A")
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
@@ -38,7 +30,6 @@ export const deletePost = async (req, res) => {
 
   try {
     await post.delete()
-    console.log("DELETING post: ", postId)
     res.status(200)
   } catch (error) {
     res.status(409).json({ message: error.message })
